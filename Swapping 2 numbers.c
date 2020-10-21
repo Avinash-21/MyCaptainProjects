301a9ba1a0cc1c3903bf7438dc72c2786d17d0c3
Avinash-21 <73236292+Avinash-21@users.noreply.github.com>
2020-10-21 19:09:37 +0530

    Program to swap 2 numbers
    Simple code to swap 2 number taken from the user.

---
+++ Swapping 2 numbers.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+main()
+{
+	float a,b;
+	printf("Enter the first number:");
+	scanf("%f",&a);
+	printf("Enter the second number:");
+	scanf("%f",&b);
+
+	/*Swapping 2 numbers*/
+	printf("After Swapping successfully the results are ....\n");
+	printf("First number is %f\n",b);
+	printf("Second number is %f",a);
+	return 0;
+}
